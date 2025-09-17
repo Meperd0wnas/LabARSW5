578a7def04747d254c4dc47e80a52cbfe30865fc
Daniel <daniel.ruge-g@mail.escuelaing.edu.co>
2025-09-17 12:41:12 -0500

    BlueprintAPIController como API

--- README.md
+++ README.md
@@ -15,3 +15,9 @@ Integración del proyecto base suministrado sin archivos de configuración
 agregamos un constructor en InMemoryBlueprintPersistence que inicialice algunos planos de ejemplo al arrancar la aplicación cumpliendo la condición de que dos sean del mismo autor.
 
 ![alt text](./img/media/image1.png)
+
+### 3
+
+Modifique la clase BlueprintAPIController en base al ejemplo dado, la clase usa @RestController para exponer endpoints REST que devuelven directamente JSON, con @RequestMapping("/blueprints") se define la ruta base del recurso. A través de @Autowired se inyecta el servicio BlueprintsServices, y el método manejadorGetRecursoBlueprints() atiende las peticiones GET /blueprints devolviendo todos los planos en JSON. Finalmente, ResponseEntity permite enviar tanto los datos como el código HTTP correspondiente (200 OK en éxito o 404 en error).
+
+![alt text](./img/media/image3.png)
--- src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
+++ src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
@@ -1,19 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.eci.arsw.blueprints.controllers;
 
-/**
- *
- * @author hcadavid
- */
+import edu.eci.arsw.blueprints.model.Blueprint;
+import edu.eci.arsw.blueprints.services.BlueprintsServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping(value = "/blueprints")
 public class BlueprintAPIController {
-    
-    
-    
-    
-    
+
+    @Autowired
+    private BlueprintsServices services;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<?> manejadorGetRecursoBlueprints() {
+        try {
+            Set<Blueprint> data = services.getAllBlueprints();
+            return new ResponseEntity<>(data, HttpStatus.OK);
+        } catch (Exception ex) {
+            Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("Error al obtener los planos", HttpStatus.NOT_FOUND);
+        }
+    }
 }
 
